af01a11505b80a3689faa99706cf15b02415246f
nitinpinnamaneni <n_pinnamaneni@u.pacific.edu>
2026-02-12 19:43:02 -0500

    Subtask repo

--- src/main/java/com/revature/revado/repository/SubtaskRepository.java
+++ src/main/java/com/revature/revado/repository/SubtaskRepository.java
@@ -1,6 +1,15 @@
 package com.revature.revado.repository;
 
+import com.revature.revado.entity.Subtask;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
 /**
-@author $ {USER}
-**/public class SubtaskRepository {
+ * @author $ {USER}
+ **/
+@Repository
+public interface SubtaskRepository extends JpaRepository<Subtask, UUID> {
+
 }

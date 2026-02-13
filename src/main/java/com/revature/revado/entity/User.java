09c34482929f3e71fcd8da146813c810423ab30b
nitinpinnamaneni <n_pinnamaneni@u.pacific.edu>
2026-02-12 19:37:26 -0500

    User

--- src/main/java/com/revature/revado/entity/User.java
+++ src/main/java/com/revature/revado/entity/User.java
@@ -1,5 +1,6 @@
 package com.revature.revado.entity;
 
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,8 +10,11 @@ import java.util.UUID;
  * @author $ {USER}
  **/
 @Data
-@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String username;
     private String password;

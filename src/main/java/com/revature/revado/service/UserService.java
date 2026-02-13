b839432181ebbac6efccbc7afa36dcc5a50b7e09
nitinpinnamaneni <n_pinnamaneni@u.pacific.edu>
2026-02-12 19:45:09 -0500

    User service

--- src/main/java/com/revature/revado/service/UserService.java
+++ src/main/java/com/revature/revado/service/UserService.java
@@ -1,6 +1,14 @@
 package com.revature.revado.service;
 
+import com.revature.revado.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
 /**
-@author $ {USER}
-**/public class UserService {
+ * @author $ {USER}
+ **/
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepo;
 }

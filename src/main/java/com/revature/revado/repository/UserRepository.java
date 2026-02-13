23ee4d04be11ebe1b7af794cc6e26c43f4a61395
nitinpinnamaneni <n_pinnamaneni@u.pacific.edu>
2026-02-12 19:44:11 -0500

    User repo

--- src/main/java/com/revature/revado/repository/UserRepository.java
+++ src/main/java/com/revature/revado/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.revature.revado.repository;
 
+import com.revature.revado.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
 
 /**
  * @author $ {USER}
  **/
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, UUID> {
 }

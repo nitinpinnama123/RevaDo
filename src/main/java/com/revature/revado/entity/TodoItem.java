d11165499d7333114c1bf446dcdc21a8437438a2
nitinpinnamaneni <n_pinnamaneni@u.pacific.edu>
2026-02-13 20:49:40 -0500

    Todo item

--- src/main/java/com/revature/revado/entity/TodoItem.java
+++ src/main/java/com/revature/revado/entity/TodoItem.java
@@ -1,9 +1,14 @@
 package com.revature.revado.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.sql.Time;
+import java.util.UUID;
 
 /**
  * @author $ {USER}
@@ -12,7 +17,14 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "items")
 public class TodoItem {
-    private int id;
+    private UUID id;
     private String description;
-    private boolean isComplete;
+    private String status;
+    @ManyToOne
+    @JoinColumn(name = "created_by_id")
+    private User createdBy;
+    private Time timeCreated;
+    @ManyToOne
+    @JoinColumn(name = "assigned_to_id")
+    private User assignedTo;
 }

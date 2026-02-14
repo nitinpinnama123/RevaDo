c7d3b308ab9946654e737641a1f7a5536e5dd625
nitinpinnamaneni <n_pinnamaneni@u.pacific.edu>
2026-02-13 20:49:11 -0500

    Subtask

--- src/main/java/com/revature/revado/entity/Subtask.java
+++ src/main/java/com/revature/revado/entity/Subtask.java
@@ -1,9 +1,13 @@
 package com.revature.revado.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 /**
  * @author $ {USER}
@@ -12,7 +16,10 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "subtasks")
 public class Subtask {
-    private int subtaskId;
-    private boolean isComplete;
+    private UUID subtaskId;
     private String subtaskDesc;
+    private String status;
+    @ManyToOne
+    @JoinColumn(name = "todo_item_id")
+    private TodoItem todoItem;
 }

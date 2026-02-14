236792b918bc3fc64a87145742032ee8fd201af6
nitinpinnamaneni <n_pinnamaneni@u.pacific.edu>
2026-02-13 22:25:31 -0500

    User repo

--- src/main/java/com/revature/revado/repository/UserRepository.java
+++ src/main/java/com/revature/revado/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.revature.revado.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -11,4 +12,7 @@ import java.util.UUID;
  **/
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
+
